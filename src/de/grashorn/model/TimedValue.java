b6d9a028db3cd24b6b0733cf55e5bac4b5547819
Philipp Grashorn <contact.pgrashorn@googlemail.com>
2018-03-02 10:08:55 +0100

    changed model

--- src/de/grashorn/model/TimedValue.java
+++ src/de/grashorn/model/TimedValue.java
@@ -1,4 +1,11 @@
 package de.grashorn.model;
 
 public class TimedValue {
+    public long time;
+    public double value;
+
+    public TimedValue(long time, double value) {
+        this.time = time;
+        this.value = value;
+    }
 }
--- src/de/grashorn/model/de/grashorn/model/entities/GraphColumn.java
+++ src/de/grashorn/model/de/grashorn/model/entities/GraphColumn.java
@@ -1,4 +1,37 @@
 package de.grashorn.model.de.grashorn.model.entities;
 
+import de.grashorn.model.TimedValue;
+
 public class GraphColumn {
+    int count;
+    public TimedValue min;
+    public TimedValue max;
+
+
+    public GraphColumn() {
+//        this.min = new TimedValue();
+//        this.max = new
+
+    }
+
+    public void addValue(TimedValue tv) {
+        count++;
+        if (count == 1) {
+            max = tv;
+        } else if (count == 2) {
+            if (tv.value > max.value) {
+                TimedValue temp;
+                temp = max;
+                max = tv;
+                min = temp;
+            } else {
+                min = tv;
+            }
+        } else if (count > 2) {
+            if (tv.value > max.value)
+                max = tv;
+            else if (tv.value < min.value)
+                min = tv;
+        }
+    }
 }
